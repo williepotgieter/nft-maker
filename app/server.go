03a2805baa9dc02210d45eedfceadf07202f9a84
Willie Potgieter <willie658@gmail.com>
2022-04-04 00:11:14 +0200

    set up basic swagger implementation

--- app/server.go
+++ app/server.go
@@ -7,6 +7,10 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+
+	_ "nft-maker/docs"
+
+	swagger "github.com/arsmn/fiber-swagger/v2"
 )
 
 type RestApi struct {
@@ -29,9 +33,23 @@ func NewRestAPI(name string, c *DBConn) *RestApi {
 	return &RestApi{c, app, v}
 }
 
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:3000
+// @BasePath /
+
 func (s *RestApi) Run(port uint16) {
 	// Add routes
 	s.setupV1Routes()
+
+	// Add swagger
+	s.setupSwagger()
 
 	s.app.Listen(fmt.Sprintf("localhost:%d", port))
 }
@@ -47,4 +65,8 @@ func (s *RestApi) setupV1Routes() {
 	users.Get("/:id", s.HandleGetUser)
 	users.Patch("/:id/password", s.HandleUpdateUserPassword)
 	users.Delete("/:id", s.HandleDeleteUser)
+}
+
+func (s *RestApi) setupSwagger() {
+	s.app.Get("/swagger/*", swagger.HandlerDefault)
 }
